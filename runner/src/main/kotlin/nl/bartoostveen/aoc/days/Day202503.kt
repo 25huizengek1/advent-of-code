30295c44dbb1e35d8dbe73d8b7f930c355a8c065
Bart Oostveen <bart.oos.2006@gmail.com>
2025-12-03 21:34:20 +0100

    2025: Day 03

--- runner/src/main/kotlin/nl/bartoostveen/aoc/days/Day202503.kt
+++ runner/src/main/kotlin/nl/bartoostveen/aoc/days/Day202503.kt
@@ -3,4 +3,26 @@ package nl.bartoostveen.aoc.days
 import nl.bartoostveen.aoc.runner.puzzle
 
 val day202503 = puzzle {
+    val banks = lines
+        .map { line ->
+            line
+                .split("")
+                .mapNotNull { it.toIntOrNull() }
+        }
+
+    fun solve(n: Int) = banks.sumOf { bank ->
+        var bank = bank
+        var joltage = 0L
+
+        for (digit in n downTo 1) {
+            val index = (0..bank.size - digit).maxBy(bank::get)
+            joltage = 10 * joltage + bank[index]
+            bank = bank.drop(index + 1)
+        }
+
+        joltage
+    }
+
+    partOne = solve(2)
+    partTwo = solve(12)
 }
